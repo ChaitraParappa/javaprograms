e1189d075614ba4513d1a4bf9eb4e10f6f0cb584
Chaitra <chaitra.p321@gmail.com>
2017-03-29 11:35:56 +0530

    success

---
+++ Factors.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class Factors
+{
+    public static void main(String[] args)
+    {
+     Scanner sc= new Scanner(System.in);
+     System.out.println("Enter the  nth number");
+     int n=sc.nextInt();
+    
+      for(int j=2;j<=n;j++)
+       {
+      if(isPrime(j))
+        {
+         if(j*j<=n)
+        System.out.println(j);
+         }
+        
+
+        }
+      }        
+         
+      static boolean isPrime(int num)
+        {
+          int i=2;
+          while(i<=num/2)
+          {
+            if(num%i==0)
+            return false;
+            i++;
+          }
+         return true;
+        }
+}
+
+     
